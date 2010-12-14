59ebb5aee09f5f9bf65af37d70a8c105c67afc60
Nick Andrew <nick@nick-andrew.net>
2010-12-15 02:15:42 +1100

    Add lib/XBee/TTY.pm

---
+++ lib/XBee/TTY.pm
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+#
+
+package XBee::TTY;
+
+use strict;
+
+use IO::File qw(O_RDWR);
+
+sub new {
+	my ($class, $filename) = @_;
+
+	my $self = {
+		filename => $filename,
+	};
+
+	bless $self, $class;
+
+	if (! -c $filename) {
+		die "Not a character special file: $filename";
+	}
+
+	my $fh = IO::File->new($filename, O_RDWR());
+	if (! $fh) {
+		die "Unable to open $filename for read-write - $!";
+	}
+
+	$self->{fh} = $fh;
+	$self->{fd} = $fh->fileno();
+
+	return $self;
+}
+
+sub socket {
+	my ($self) = @_;
+
+	return $self->{fh};
+}
+
+1;
