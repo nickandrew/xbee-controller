ca94884c8abb88c16393d1d596726d71d9bed6f7
Nick Andrew <nick@nick-andrew.net>
2011-02-24 13:47:26 +1100

    Set 9600 bps, raw mode on TTY

--- lib/Selector/TTY.pm
+++ lib/Selector/TTY.pm
@@ -1,11 +1,13 @@
 #!/usr/bin/perl -w
 #
+#  TTY access library
 
 package Selector::TTY;
 
 use strict;
 
 use IO::File qw(O_RDWR);
+use POSIX qw();
 
 sub new {
 	my ($class, $filename) = @_;
@@ -25,6 +27,8 @@ sub new {
 		die "Unable to open $filename for read-write - $!";
 	}
 
+	setupDevice($fh);
+
 	$self->{fh} = $fh;
 	$self->{fd} = $fh->fileno();
 
@@ -35,6 +39,29 @@ sub socket {
 	my ($self) = @_;
 
 	return $self->{fh};
+}
+
+# ---------------------------------------------------------------------------
+# Setup a TTY device, set 9600 bps etc
+# ---------------------------------------------------------------------------
+
+sub setupDevice {
+	my ($dev) = @_;
+
+	my $fd = $dev->fileno();
+
+	my $termios = POSIX::Termios->new();
+	$termios->getattr($fd);
+	my $c_cflag = $termios->getcflag();
+	my $c_lflag = $termios->getlflag();
+	my $c_oflag = $termios->getoflag();
+
+	$termios->setispeed( &POSIX::B9600 );
+	$termios->setospeed( &POSIX::B9600 );
+	my $l_off = ( &POSIX::ECHO | &POSIX::ECHONL | &POSIX::ICANON | &POSIX::ISIG | &POSIX::IEXTEN );
+	my $o_off = ( &POSIX::OPOST );
+	$termios->setlflag( $c_lflag & ~$l_off );
+	$termios->setoflag( $c_oflag & ~$o_off );
 }
 
 1;
