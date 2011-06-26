73900c77d6f5cf1079d96497b9da970a4fc9b7ae
Nick Andrew <nick@nick-andrew.net>
2011-06-26 23:34:08 +1000

    Add usage comments

--- bin/xbee-cmd.pl
+++ bin/xbee-cmd.pl
@@ -1,12 +1,19 @@
 #!/usr/bin/perl -w
 #   vim:sw=4:ts=4:
 #
-#  Copyright (C) 2010, Nick Andrew <nick@nick-andrew.net>
+#  Copyright (C) 2010-2011, Nick Andrew <nick@nick-andrew.net>
 #  Licensed under the terms of the GNU General Public License, Version 3
 #
-#  Send a command to an XBee in API mode
+#  Send a frame to an XBee in API mode.
+#  This can be used to send any type of API frame; the entire contents of the frame
+#  must be supplied in hex on the command line.
 #
 #  Usage: xbee-cmd.pl -h host:port xx xx xx ...
+#
+#  Example: Send an AT 'NI' command:
+#     xbee-cmd.pl -h host:port 08 11 4e 49
+#  Example: Read the firmware version:
+#     xbee-cmd.pl -h host:port 08 11 56 52
 
 use strict;
 
