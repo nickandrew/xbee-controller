921237aa9e11e56d00a4b8b614a0fe926132e874
Nick Andrew <nick@nick-andrew.net>
2011-07-01 23:53:36 +1000

    Add a script to manipulate an XBee node

---
+++ bin/xbee-node.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl -w
+#  vim:sw=4:ts=4:
+#
+#  Copyright (C) 2011, Nick Andrew <nick@nick-andrew.net>
+#  Licensed under the terms of the GNU General Public License, Version 3
+#
+#  Modify an XBee Node.
+#  At present this command can only set the Node Identity variable.
+#
+#  Usage:
+#        xbee-node.pl [-h host:port] [-n xxx:xxx] node_identity 'name'
+#
+#  Options:
+#    -h host:port        XBee server host:port
+
+use strict;
+
+use Getopt::Std qw(getopts);
+
+use XBee::Client qw();
+use XBee::Network qw();
+use XBee::AT::ZB qw();
+
+use vars qw($opt_h $opt_n);
+
+getopts('h:n:');
+
+$opt_h ||= '127.0.0.1:7862';
+
+$opt_n || die "Need option -n xxx:xxx";
+
+my $xcl = XBee::Client->new($opt_h);
+
+if (!defined $xcl) {
+	die "Unable to connect to xbee server";
+}
+
+my $network = XBee::Network->new(
+	XBee::AT::ZB->new(),
+	$xcl);
+
+my $node = XBee::Node->new($opt_n, $network);
+
+while (@ARGV) {
+	my $command = shift @ARGV;
+
+	if ($command eq 'node_identity') {
+		my $arg = shift @ARGV;
+		if ($arg) {
+			$node->setNodeID($arg, 1);
+		}
+	}
+	else {
+		print "Unknown command: $command\n";
+	}
+}
+
+exit(0);
