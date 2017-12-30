7487396ed51599afef237bf363c065d061925841
Nick Andrew <nick@nick-andrew.net>
2017-12-30 23:19:08 +1100

    Introduce random errors in frame to test error handling
    Introduce a small rate of byte errors in received data. The error
    could occur in any byte or bytes of a frame. An erroneous frame
    needs to be caught and the error reported to STDERR with enough
    detail to reproduce what happened (show all the bytes received,
    not find out what the correct packet should have been).

--- lib/TullNet/XBee/API/Frame.pm
+++ lib/TullNet/XBee/API/Frame.pm
@@ -45,6 +45,7 @@ sub new {
 	my $self = {
 		debug => 0,
 		data => undef,
+		index => 0,
 		leading_junk => '',
 		l_msb => undef,
 		packet_max_length => 255,
@@ -72,14 +73,23 @@ sub addData {
 	my ($self, $buf) = @_;
 
 	my $state = $self->{'state'};
+	my $index = $self->{'index'}; # Index of byte number within frame (0x7e byte is index 0)
 
 	foreach my $c (split(//, $buf)) {
+		# Sometimes, introduce an error
+		if (int(rand(200)) == 0) {
+			my $o_c = $c;
+			$c = chr(int(rand(256)));
+			$self->error("Corrupted byte index %d from %02x to %02x",
+				$index, ord($o_c), ord($c));
+		}
 
 		if ($state == 0) {
 			if ($c eq chr(0x7e)) {
 				$self->{'data'} = undef;
 				$self->{'done'} = 0;
 				$self->{'cksum'} = 0;
+				$index = 1; # Next byte is index 1
 				$state = 1;
 				if ($self->{'leading_junk'} ne '') {
 					$self->printHex("Skipping junk pre frame start:", $self->{'leading_junk'});
@@ -91,10 +101,12 @@ sub addData {
 		}
 		elsif ($state == 1) {
 			$self->{'l_msb'} = ord($c);
+			$index ++;
 			$state = 2;
 		}
 		elsif ($state == 2) {
 			$self->{'l_lsb'} = ord($c);
+			$index ++;
 			my $length = ($self->{'l_msb'} << 8) + $self->{'l_lsb'};
 			if ($length > $self->{'packet_max_length'}) {
 				# Don't allow arbitrarily long packets
@@ -111,6 +123,7 @@ sub addData {
 			$self->{'data'} .= $c;
 			$self->{'cksum'} += ord($c);
 			$self->{'to_read'} --;
+			$index ++;
 			if ($self->{'to_read'} == 0) {
 				$state = 4;
 			}
@@ -130,14 +143,16 @@ sub addData {
 			}
 
 			$state = 0;
+			$index = 0;
 		}
 		else {
 			die "Illegal state $state";
 		}
 	}
 
-	# Remember state within frame for next time
+	# Remember state and index within frame for next time
 	$self->{'state'} = $state;
+	$self->{'index'} = $index;
 
 	return 1;
 }
